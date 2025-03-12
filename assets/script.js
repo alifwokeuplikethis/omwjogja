6606e1467f0c81fea98dabf0e3a077d3cf67483d
Alief <alief.nug21@gmail.com>
2025-03-12 11:28:15 +0700

    test

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     const container = document.querySelector('.container');
-    const sections = document.querySelectorAll('.snap-section, .no-snap-section');
-    const lastSection = sections[sections.length - 1];
-
-    container.addEventListener('scroll', () => {
-        
-        const lastSectionTop = lastSection.offsetTop;
-        const scrollPosition = container.scrollTop + container.clientHeight;
-
-        if (scrollPosition >= lastSectionTop) {
-            container.style.scrollSnapType = 'none';
-        } else {
-            container.style.scrollSnapType = 'y mandatory';
-        }
-    });
+    container.style.scrollSnapType = 'none'; // Memastikan tidak ada efek snap
 });
