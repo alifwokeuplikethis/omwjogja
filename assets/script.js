20d3979c40c8575a1ddfbd7daee64cf5d6ea5877
Alief <alief.nug21@gmail.com>
2025-03-12 11:32:45 +0700

    test 2

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container');
-    container.style.scrollSnapType = 'none'; // Memastikan tidak ada efek snap
-});
+var alif
